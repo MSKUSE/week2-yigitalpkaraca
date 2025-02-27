ee4810a725a4f38f3c1883f49aee2a64e76f3245
yigitalpkaraca <yigitalpkaraca@posta.mu.edu.tr>
2025-02-27 13:12:51 +0300

    Dt is added

---
+++ src/Dt.java
@@ -0,0 +1,15 @@
+public class Dt {
+
+    public static void main(String[] args) {
+        int grade = 1;
+        double grade_double = 1.5;
+        char first = 'A' ;
+        boolean isok = true;
+
+        System.out.println(grade);
+        System.out.println(grade_double);
+        System.out.println(first);
+        System.out.println(isok);
+    }
+
+}
